19c022985e694f50e746e026a145add2eda8da4d
bencroskery <ben@irtech.com>
2016-01-15 01:41:50 -0500

    Better Hover Exclusion
    - Instead of using JS to exclude the subtext from the icon hover CSS
    pointer events can easily be used
    - Whole page fades once again on top of scaling
    - Icon subtext can be highlighted/selected

--- main.js
+++ main.js
@@ -1,24 +1,9 @@
 "use strict";
 
-var icons = document.getElementsByClassName("icon");
-var inter;
-for (var i = 0; i < icons.length; i++) {
-    icons[i].onmouseover = function() {
-        var el = this;
-        clearTimeout(inter);
-        el.lastChild.style.display = "block";
-        setTimeout(function() {el.lastChild.className = 'open'}, 10);
+// Fade in body.
+setTimeout(function() {document.body.style.opacity = 1}, 0);
 
-    };
-
-    icons[i].onmouseout = function() {
-        var el = this;
-        inter = setTimeout(function() {
-            el.lastChild.className = '';
-            setTimeout(function() {el.lastChild.style.display = "none"}, 300)}, 50);
-    };
-}
-
+// Scale in all links.
 var a = document.getElementsByTagName('a');
 var j = 0;
 function op() {
